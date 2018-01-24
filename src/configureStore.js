be86a54afbd5829d1e3a6acb1df0f282e8f383cf
Катышев Никита Петрович <OUT-Katyishev-NP@mail.ca.sbrf.ru>
2018-01-24 11:09:18 +0300

    add logging to dispatch

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,9 +3,23 @@ import reducers from "./reducers/reducers";
 import {loadState, saveState} from "./localStorage";
 import throttle from "lodash/throttle";
 
+const addLoggingToDispatch = store => {
+    const rawDispatch = store.dispatch;
+    return action => {
+        console.group(action.type);
+        console.log("prevState: " + store.getState());
+        console.log("action: " + action);
+        const returnValue = rawDispatch(action);
+        console.log("nextState: " + store.getState());
+        console.groupEnd(action.type);
+        return returnValue;
+    }
+};
+
 export default () => {
     const persistedState = loadState();
     const store = createStore(reducers, persistedState);
+    store.dispatch = addLoggingToDispatch(store);
 
     store.subscribe(throttle(() => saveState({
         todos: store.getState().todos
